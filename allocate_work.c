f608bc4f71cc3ee1e7ce9cf089a87b43cc52fd00
absmaq <computacao.em.foco@outlook.com>
2017-04-24 17:52:42 -0300

    Update allocate_work.c

--- allocate_work.c
+++ allocate_work.c
@@ -1,3 +1,22 @@
+/*
+
+There is a bunch of tasks, each have different time to complete, task is independent, and then there are some workers,
+How to allocate tasks to these workers to minimize the total time to complete all the task. The tasks can be randomly picked from the task list.
+Example
+Task: 2,2,3,7, 1
+Worker: 2.
+Return 8, because the first worker can work on the first three tasks : 2 + 2 + 3 = 7, and the second worker can work on the last two tasks: 
+7 + 1 = 8, so the total time to finish all the task is 8.
+public int getMini(int[] tasks, int k)
+
+*/
+
+/* Greedy algorithm: Each worker take the task with highest value after they finish the previous task
+ * It will have a auxiliary array that holds the times of tasks for each worker
+ * Then, this array is rearranged so that the worker holding the task with shortest duration goes to the first position and receive the next task
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
